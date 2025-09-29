d0de3757be8bf824432e5a5a4f1fff32822a58a7
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-09-29 11:12:20 +0300

    Inheritance

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
 class Fruit {
-    public $name;
-    public $color;
+  public $name;
+  public $color;
 
-    public function __construct($name, $color)
-    {
-        $this->name = $name;
-        $this->color = $color;
-    }
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
 
-    public function get_name() {
-        return $this->name;
-    }
+  public function intro(){
+    echo "The Fruit name is {$this->name} and the color is {$this->color}.";
+  }
+}
 
 
-    public function get_color() {
-        return $this->color;
+class Strawberry extends Fruit {
+    public function message() {
+        echo "Am I a fruit or a berry? <br>";
     }
 }
 
-$apple = new Fruit("Apple", "Green");
+$strawberry = new Strawberry("Strawbery", "red");
+$strawberry->message();
+$strawberry->intro();
