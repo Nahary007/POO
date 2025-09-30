eb332b505c4a432b16e78c6496b119dadcdf172c
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-09-30 11:30:15 +0300

    Multiple traits

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <?php
 
 trait message1 {
-  public function msg1() {
-    echo "Hi there!";
+  public  function msg1() {
+    echo "OOP is fun! ";
   }
 }
 
-class Welcome {
+trait message2 {
+  public function msg2() {
+    echo "OOP reduces code duplication!";
+  }
+}
+
+class Welcome1 {
   use message1;
 }
 
-$greeting = new Welcome();
-$greeting->msg1();
+class Welcome2 {
+  use message1, message2;
+}
+
+$obj = new Welcome1();
+$obj->msg1();
+echo "<br>";
+
+$obj2 = new Welcome2();
+$obj2->msg1();
+$obj2->msg2();
